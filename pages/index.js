2173d35a71d62f1c0335321ac2911bf8afa5f6b2
andrewpjames13 <andrew.james@acumendigital.com>
2020-03-09 09:31:59 -0600

    Get latest BTC price from blockchain.info

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import axios from 'axios';
 import TransactionForm from 'components/TransactionForm';
 
 const Container = styled.div`
@@ -25,12 +26,20 @@ const Title = styled.h1`
 `;
 
 const Index = () => {
+  const [currentBtcPrice, setCurrentBtcPrice] = useState();
+  useEffect(() => {
+    axios.get('https://blockchain.info/ticker')
+      .then(res => setCurrentBtcPrice(res.data.USD.last))
+      .catch(res => setCurrentBtcPrice(null))
+  }, []);
+
   return (
     <Container className="contianer-fluid">
       <div className="row center-xs">
         <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 24"><path d="M17,24H7L5,8H19Zm-6.2-5.5a.7.7,0,0,1,.7.7.7.7,0,0,1-.7.8.7.7,0,0,1-.8-.8A.7.7,0,0,1,10.8,18.5ZM14,16a1,1,0,1,1-1,1A.9.9,0,0,1,14,16Zm-3.5-3A1.5,1.5,0,1,1,9,14.5,1.5,1.5,0,0,1,10.5,13Zm1.7-9,2.1-4,1.3.6L13.8,4H18c0,.9,1.1,1.6,2,1.6V7H4V5.6c.9,0,2-.7,2-1.6Z" transform="translate(-4)"/></Svg>
         <Title className="col-xs-12">Sam's Lemonade Stand</Title>
         <h5 className="col-xs-12">When life gives you Bitcoin, make lemonade!</h5>
+        {currentBtcPrice && <p className="col-xs-12">Current btc price ${currentBtcPrice}</p>}
       </div>
       <TransactionForm />
     </Container>
